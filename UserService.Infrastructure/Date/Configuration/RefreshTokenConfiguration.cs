19c097f45b3bc1613ef08e2859c7c9b7c2aebfef
Pavel_Stykachov <111525871+Candymanzz@users.noreply.github.com>
2025-11-16 15:25:15 +0300

    feat: add configuration for refresh token

---
+++ UserService.Infrastructure/Date/Configuration/RefreshTokenConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UserService.Domain.Models;
+
+namespace UserService.Infrastructure.Date.Configuration
+{
+    internal class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<RefreshToken> builder)
+        {
+            builder.ToTable("RefreshTokens");
+
+            builder.HasKey(r => r.Id);
+
+            builder.Property(r => r.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(r => r.Token)
+                .IsRequired();
+
+            builder.Property(r => r.ExpiresAt)
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+
+            builder.Property(r => r.CreatedAt)
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+
+            builder.Property(r => r.RevokedAt)
+                .IsRequired(false);
+
+            builder.HasOne(r => r.User)
+                .WithMany(u => u.RefreshTokens)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
