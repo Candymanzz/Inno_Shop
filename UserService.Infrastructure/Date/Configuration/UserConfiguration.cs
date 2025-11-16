7a3b740f400b6806fecf1eef6c92b03e015e8c7a
Pavel_Stykachov <111525871+Candymanzz@users.noreply.github.com>
2025-11-16 15:24:43 +0300

    Update UserConfiguration.cs

--- UserService.Infrastructure/Date/Configuration/UserConfiguration.cs
+++ UserService.Infrastructure/Date/Configuration/UserConfiguration.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using UserService.Domain.Model;
+using UserService.Domain.Models;
 
 namespace UserService.Infrastructure.Date.Configuration
 {
