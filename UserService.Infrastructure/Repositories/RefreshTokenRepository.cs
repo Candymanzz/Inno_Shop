ee763ec5920f4ebefc5a206570280411fdac3c0c
Pavel_Stykachov <111525871+Candymanzz@users.noreply.github.com>
2025-11-22 02:37:58 +0300

    feat: add repositories

---
+++ UserService.Infrastructure/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Domain.Interfaces;
+using UserService.Domain.Models;
+using UserService.Infrastructure.Date;
+
+namespace UserService.Infrastructure.Repositories
+{
+    internal class RefreshTokenRepository : IRefreshTokenRepository //!!!доделать
+    {
+        private readonly AppDbContext appDbContext;
+        public RefreshTokenRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public async Task AddAsync(RefreshToken refreshToken)
+        {
+            await appDbContext.RefreshTokens.AddAsync(refreshToken);
+            await SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(RefreshToken refreshToken)
+        {
+            appDbContext.RefreshTokens.Remove(refreshToken);
+            await SaveChangesAsync();
+        }
+
+        public async Task<RefreshToken?> GetByTokenAsync(string token)
+        {
+            return await appDbContext.RefreshTokens.FirstOrDefaultAsync(r => r.Token == token);
+        }
+
+        public async Task<IEnumerable<RefreshToken>> GetRefreshTokenByUserIdAsync(Guid userId)
+        {
+            return await appDbContext.RefreshTokens.Where(r => r.UserId == userId).ToListAsync();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await appDbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(RefreshToken token)
+        {
+            appDbContext.RefreshTokens.Update(token);
+            await SaveChangesAsync();
+        }
+    }
+}
---
+++ UserService.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Domain.Interfaces;
+using UserService.Domain.Models;
+using UserService.Infrastructure.Date;
+
+namespace UserService.Infrastructure.Repositories
+{
+    internal class UserRepository : IUserRepository //!!!доделать
+    {
+        private readonly AppDbContext appDbContext;
+
+        public UserRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await appDbContext.Users.AddAsync(user);
+            await SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(User user)
+        {
+            appDbContext.Users.Remove(user);
+            await SaveChangesAsync();
+        }
+
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await appDbContext.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<User?> GetByIdAsync(Guid id)
+        {
+            return await appDbContext.Users.FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<(IEnumerable<User> Items, int Total)> GetPagedAsync(int page, int pageSize, string? q = null)
+        {
+            IQueryable<User> query = appDbContext.Users;
+
+            if (!string.IsNullOrWhiteSpace(q))
+            {
+                query = query.Where(u => u.Email.Contains(q) || u.FullName.Contains(q));
+            }
+
+            int total = await query.CountAsync();
+
+            List<User> items = await query
+                .OrderBy(u => u.FullName)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return (items, total);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await appDbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            appDbContext.Users.Update(user);
+            await SaveChangesAsync();
+        }
+    }
+}
