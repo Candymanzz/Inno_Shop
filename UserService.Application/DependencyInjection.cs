b4e778eb489db82a2b7751e607f0acc8ba5f20e3
Pavel_Stykachov <111525871+Candymanzz@users.noreply.github.com>
2025-11-22 02:35:34 +0300

    Create DependencyInjection.cs

---
+++ UserService.Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using UserService.Application.Services;
+using UserService.Domain.Interfaces;
+
+namespace UserService.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration) //перерегать все ди
+        {
+            services.Configure<JwtOptions>(configuration.GetSection(nameof(JwtOptions)));
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPasswordHasher, PasswordHasher>();
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
