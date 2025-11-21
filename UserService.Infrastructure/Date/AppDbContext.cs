c49e14e2b3f20c5682b37add3261044e7bda2681
Pavel_Stykachov <111525871+Candymanzz@users.noreply.github.com>
2025-11-22 02:37:36 +0300

    Update AppDbContext.cs

--- UserService.Infrastructure/Date/AppDbContext.cs
+++ UserService.Infrastructure/Date/AppDbContext.cs
@@ -4,7 +4,7 @@ using UserService.Infrastructure.Date.Configuration;
 
 namespace UserService.Infrastructure.Date
 {
-    internal class AppDbContext : DbContext
+    public class AppDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
         public DbSet<RefreshToken> RefreshTokens { get; set; }
