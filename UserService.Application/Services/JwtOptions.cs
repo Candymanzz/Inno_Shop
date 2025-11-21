83ad811452f894e0e8640b37a1941630d5372611
Pavel_Stykachov <111525871+Candymanzz@users.noreply.github.com>
2025-11-22 02:36:37 +0300

    Create JwtOptions.cs

---
+++ UserService.Application/Services/JwtOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace UserService.Application.Services
+{
+    internal class JwtOptions
+    {
+        public string SecretKey { get; set; } = string.Empty;
+        public int ExpiresHours { get; set; }
+    }
+}
