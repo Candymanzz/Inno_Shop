2e095dea26094a75e502f296567ffffa56fae767
Pavel_Stykachov <111525871+Candymanzz@users.noreply.github.com>
2025-11-22 02:35:06 +0300

    Create ApiExtensions.cs

---
+++ UserService.API/Extensions/ApiExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace UserService.API.Extensions
+{
+    public class ApiExtensions
+    {
+        public static void AddApiAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+    }
+}
