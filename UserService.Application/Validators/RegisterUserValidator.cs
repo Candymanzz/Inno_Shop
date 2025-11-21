5e3eaff9f18960036f18907042fc766b514c7bd6
Pavel_Stykachov <111525871+Candymanzz@users.noreply.github.com>
2025-11-22 02:36:51 +0300

    Create RegisterUserValidator.cs

---
+++ UserService.Application/Validators/RegisterUserValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using UserService.Application.Commands.Users;
+
+namespace UserService.Application.Validators
+{
+    public class RegisterUserValidator : AbstractValidator<RegisterUserCommand>
+    {
+        public RegisterUserValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .MinimumLength(10);
+
+            RuleFor(x => x.FullName)
+                .NotEmpty()
+                .MinimumLength(2);
+        }
+    }
+}
