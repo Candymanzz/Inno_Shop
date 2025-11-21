9cc7e4202b5875dab5e8f286ba45df245bb3cc75
Pavel_Stykachov <111525871+Candymanzz@users.noreply.github.com>
2025-11-22 02:36:43 +0300

    Create TokenService.cs

---
+++ UserService.Application/Services/TokenService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UserService.Domain.Interfaces;
+using UserService.Domain.Models;
+
+namespace UserService.Application.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly JwtOptions jwtOptions;
+
+        public TokenService(IOptions<JwtOptions> jwtOptions)
+        {
+            this.jwtOptions = jwtOptions.Value;
+        }
+
+        public string GenereateAccessToken(User user)
+        {
+            Claim[] claims = [new("userId", user.Id.ToString())];
+
+            SigningCredentials signingCredentials = new SigningCredentials(new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(jwtOptions.SecretKey)),
+                SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken token = new JwtSecurityToken(claims: claims,
+                signingCredentials: signingCredentials,
+                expires: DateTime.UtcNow.AddHours(jwtOptions.ExpiresHours));
+
+            string tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+            
+            return tokenValue;
+        }
+
+        public RefreshToken GetRefreshToken(Guid userId, TimeSpan ttl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
