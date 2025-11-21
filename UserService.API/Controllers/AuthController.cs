ffa3c04b9a31eee208764a9deb855fa40603b433
Pavel_Stykachov <111525871+Candymanzz@users.noreply.github.com>
2025-11-22 02:34:59 +0300

    feat: add controllers

---
+++ UserService.API/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using UserService.Application.Commands.Auth;
+using UserService.Application.Commands.Users;
+using UserService.Application.DTOs.AuthDTOs.RequestDTOs;
+using UserService.Application.DTOs.AuthDTOs.ResponseDTOs;
+
+namespace UserService.API.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterAsync(RegisterRequest registerRequest)
+        {
+            AuthResponse result = await mediator.Send(new RegisterUserCommand(
+                registerRequest.Email,
+                registerRequest.Password,
+                registerRequest.FullName
+                ));
+            return Ok(result);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginAsync(LoginRequest loginRequest)
+        {
+            AuthResponse result = await mediator.Send(new LoginUserCommand(
+                loginRequest.Email,
+                loginRequest.Password
+                ));
+            return Ok(result);
+        }
+
+        [HttpPost("refresh")]
+        public async Task<IActionResult> RefreshAsync(RefreshRequest refreshRequest)
+        {
+            AuthResponse result = await mediator.Send(new RefreshTokenCommand(refreshRequest.RefreshToken));
+            return Ok(result);
+        }
+    }
+}
---
+++ UserService.API/Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using UserService.Application.DTOs.UserDTOs;
+using UserService.Application.Queries.Users;
+
+namespace UserService.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetAllAsync([FromQuery] int page = 1, [FromQuery] int pageSize = 20, [FromQuery] string? q = null)
+        {
+            (IEnumerable<UserDto>? items, int total) = await mediator.Send(new GetUsersQuery(page, pageSize, q));
+            return Ok(new { items, total, page, pageSize });
+        }
+
+        [HttpGet("{id:guid}")]
+        [Authorize]
+        public async Task<IActionResult> GetByIdAsync(Guid id)
+        {
+            UserDto? userDto = await mediator.Send(new GetUserByIdQuery(id));
+            if (userDto == null)
+            {
+                return NotFound();
+            }
+
+            string? currentId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if(!User.IsInRole("Admin") && currentId != userDto.Id.ToString())
+            {
+                return Forbid();
+            }
+
+            return Ok(userDto);
+        }
+    }
+}
