a0a6d0b1b48cbaadb3f717fbb72bd3d5f9051a07
Pavel_Stykachov <111525871+Candymanzz@users.noreply.github.com>
2025-11-22 02:37:29 +0300

    feat: update models

--- UserService.Domain/Models/EmailConfirmation.cs
+++ UserService.Domain/Models/EmailConfirmation.cs
@@ -1,14 +1,12 @@
 ﻿namespace UserService.Domain.Models
 {
-    public class RefreshToken
+    public class EmailConfirmation
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public Guid UserId { get; set; }
+        public required User User { get; set; }
         public string Token { get; set; } = string.Empty;
         public DateTime ExpiresAt { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime? RevokedAt { get; set; }
-
-        public User? User { get; set; }
+        public bool IsUsed { get; set; }
     }
 }
--- UserService.Domain/Models/User.cs
+++ UserService.Domain/Models/User.cs
@@ -2,13 +2,13 @@
 {
     public class User
     {
-        public Guid Id { get; set; } 
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string FullName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
-        public string Role { get; set; } = string.Empty;
-        public bool IsActive { get; set; }
-        public bool EmailConfirmed { get; set; }
+        public string Role { get; set; } = "User"; //мне кажется это бизнес
+        public bool IsActive { get; set; } = true;
+        public bool EmailConfirmed { get; set; } = false;
 
         public List<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
     }
