b3c5b9cb5322705c8a2c9ad7f98ad3654e2e6408
Pavel_Stykachov <111525871+Candymanzz@users.noreply.github.com>
2025-11-23 23:47:35 +0300

    Update UsersController.cs

--- UserService.API/Controllers/UsersController.cs
+++ UserService.API/Controllers/UsersController.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
-using System.Threading.Tasks;
+using UserService.Application.Commands.Users;
 using UserService.Application.DTOs.UserDTOs;
 using UserService.Application.Queries.Users;
 
@@ -28,7 +28,7 @@ namespace UserService.API.Controllers
         }
 
         [HttpGet("{id:guid}")]
-        [Authorize]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> GetByIdAsync(Guid id)
         {
             UserDto? userDto = await mediator.Send(new GetUserByIdQuery(id));
@@ -38,12 +38,67 @@ namespace UserService.API.Controllers
             }
 
             string? currentId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            if(!User.IsInRole("Admin") && currentId != userDto.Id.ToString())
+            if (!User.IsInRole("Admin") && currentId != userDto.Id.ToString())
             {
                 return Forbid();
             }
 
             return Ok(userDto);
+        }
+
+        [HttpPatch("{id:guid}/set-admin")]
+        [Authorize]
+        public async Task<IActionResult> SetAdminRoleAsync(Guid id)
+        {
+            await mediator.Send(new SetAdminRoleCommand(id));
+            return Ok();
+        }
+
+        [HttpDelete("{id:guid}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteAsync(Guid id)
+        {
+            await mediator.Send(new DeleteUserCommand(id));
+            return Ok();
+        }
+
+        [HttpGet("{email}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetByEmailAsync(string email)
+        {
+            UserDto? userDto = await mediator.Send(new GetUserByEmailQuery(email));
+            return Ok(userDto);
+        }
+
+        [HttpPut("{id:Guid}")]
+        [Authorize]
+        public async Task<IActionResult> UpdateAsync(Guid id, [FromBody] UpdateUserDto updateUserDto)
+        {
+            string currentUserId = User.FindFirst("userId")!.Value;
+            string currentUserRole = User.FindFirst(ClaimTypes.Role)!.Value;
+
+            UpdateUserCommand updateUserCommand = new UpdateUserCommand(
+            TargetUserId: id,
+            FullName: updateUserDto.FullName,
+            Email: updateUserDto.Email,
+            Password: updateUserDto.Password,
+            Role: updateUserDto.Role,
+            IsActive: updateUserDto.IsActive,
+            EmailConfirmed: updateUserDto.EmailConfirmed,
+            CurrentUserId: Guid.Parse(currentUserId),
+            CurrentUserRole: currentUserRole ?? "User"
+            );
+
+            var updatedUser = await mediator.Send(updateUserCommand);
+
+            return Ok();
+        }
+
+        [HttpPatch("{id:guid}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> ChangeActivate(Guid id, [FromBody] bool activity)
+        {
+            throw new Exception();
         }
     }
 }
