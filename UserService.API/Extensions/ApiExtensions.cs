002640a623d379359f4ea59b1b845621e3266718
Pavel_Stykachov <111525871+Candymanzz@users.noreply.github.com>
2025-11-23 23:47:45 +0300

    Update ApiExtensions.cs

--- UserService.API/Extensions/ApiExtensions.cs
+++ UserService.API/Extensions/ApiExtensions.cs
@@ -1,10 +1,42 @@
-﻿namespace UserService.API.Extensions
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using UserService.Application.Services;
+
+namespace UserService.API.Extensions
 {
-    public class ApiExtensions
+    public static class ApiExtensions
     {
         public static void AddApiAuthentication(this IServiceCollection services, IConfiguration configuration)
         {
+            JwtOptions jwtOptions = configuration.GetSection("JwtOptions").Get<JwtOptions>()!;
 
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    options.TokenValidationParameters = new()
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SecretKey)),
+                        RoleClaimType = ClaimTypes.Role
+                    };
+
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            context.Token = context.Request.Cookies["a-token-cookies"];
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+
+            services.AddAuthorization();
         }
     }
 }
