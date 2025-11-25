077c5cd8c6a8957372f184ba576d53be136bb339
Pavel_Stykachov <111525871+Candymanzz@users.noreply.github.com>
2025-11-26 02:23:38 +0300

    Update DebugEmailSender.cs

--- UserService.Application/Services/DebugEmailSender.cs
+++ UserService.Application/Services/DebugEmailSender.cs
@@ -1,14 +1,35 @@
-﻿using UserService.Domain.Interfaces;
+﻿using Microsoft.Extensions.Options;
+using UserService.Domain.Interfaces;
 
 namespace UserService.Application.Services
 {
     public class DebugEmailSender : IEmailSender
     {
+        private readonly string frontendUrl;
+
+        public DebugEmailSender(IOptions<FrontendOptions> options) //http://localhost:5248
+        {
+            frontendUrl = options.Value.Url;
+        }
+
         public Task SendConfirmationEmailAsync(string to, string token)
         {
-            Console.WriteLine($"Email to={to}, token={token}");
+            string confirmationLink = $"{frontendUrl}/api/auth/confirm-email?token={token}";
+
+            Console.WriteLine($"[DEBUG] Email to={to}");
+            Console.WriteLine($"[DEBUG] Confirm your account: {confirmationLink}");
+
+            return Task.CompletedTask;
+        }
+
+        public Task SendResetPasswordEmailAsync(string to, string token)
+        {
+            string resetLink = $"{frontendUrl}/api/auth/reset-password?token={token}";
+
+            Console.WriteLine($"[DEBUG] Email to={to}");
+            Console.WriteLine($"[DEBUG] Reset your password: {resetLink}");
+
             return Task.CompletedTask;
         }
     }
-
 }
